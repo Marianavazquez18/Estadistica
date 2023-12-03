ef5bca9a61724f691539bac0e07553261d1ed00b
Marianavazquez18 <143579192+Marianavazquez18@users.noreply.github.com>
2023-12-03 11:20:27 -0600

    Graficos
    Elaboración de gráficos

---
+++ Graficos.R
@@ -0,0 +1,202 @@
+# ______________Gráficos____________
+#  Se va a trabajar con la matriz penguins
+
+#--------------------------------------------
+# Importación de matriz 
+#-------------------------------------------
+
+# Import data / from excel /browse / seleccionar archivo / aceptar
+
+#--------------------------------------
+#    Exploracion
+#-----------------------------------
+
+dim(penguins)
+colnames(penguins)
+str(penguins)
+anyNA(penguins)
+
+# Estadistica descriptiva
+summary(penguins)
+
+#-----------------------------------------
+# Configuración de la matriz
+#------------------------------------------
+
+#Visualización de una columna.
+penguins$especie
+penguins$isla
+penguins$genero
+penguins$año
+
+#1.- Convertir las variables categóticas a factores
+penguins$especie<-factor(penguins$especie, 
+                         levels=c("Adelie", "Gentoo", "Chinstrap"))
+
+
+penguins$isla<-factor(penguins$isla, 
+                      levels=c("Torgersen", "Biscoe", "Dream"))
+
+
+penguins$genero<-factor(penguins$genero, 
+                        levels=c("male", "female"))
+
+penguins$año<-factor(penguins$año, 
+                     levels=c("2007", "2008", "2009"))
+
+str(penguins)
+
+summary(penguins)
+
+View(penguins)
+
+# ---Este paso es opcional.---
+# 2.- Creamos una nueva matriz de datos donde se seleccionan
+# las columnas de la 2 a la 9.
+BD1<-penguins[,2:9]
+
+
+#---------------------------------
+#   Librerías
+#----------------------------------
+install.packages("ggplot2")
+
+library(ggplot2)
+
+
+# ------------------------------------------------
+#         Boxplot
+#--------------------------------------------------
+
+# 1.- Creación de un vector de color
+color=c("cyan","green")
+
+# 2.- Creacion del grafico
+
+BX<-ggplot(penguins, aes(x=genero, y=largo_pico_mm))+
+  geom_boxplot(fill=color)+
+  ggtitle("Boxplot")+
+  xlab("Género")+
+  ylab("largo de la aleta (mm)")+
+  theme_bw()
+
+# 3.- Visualización del boxplot
+BX
+
+#--------------------------------------------------
+#            Gráfico de barras
+#--------------------------------------------------
+
+# 1.- Creación de un vector de color
+color=c("cyan4", "lightpink", "gold")
+
+# 2.- Creación del gráfico
+GB1<-ggplot(penguins, aes(x=año))+
+  geom_bar(colour= "gray8", fill=color)+
+  ggtitle("Gráfico de Barras")+
+  xlab("Año")+
+  ylab("Frecuencias")+
+  theme_minimal()
+
+# 3.- Visualizacion del grafico
+GB1
+
+# 4.- Barras verticales
+color=c("deepskyblue", "forestgreen", "darkviolet")
+
+GB2<-ggplot(penguins, aes(x=año))+
+  geom_bar(colour= "black", fill=color)+
+  ggtitle("Gráfico de Barras")+
+  xlab("Año")+
+  ylab("Frecuencias")+
+  coord_flip()+
+  theme_minimal()
+
+# 5. Visualizacion del objeto
+GB2
+
+#--------------------------------------------------
+#           Histograma
+#---------------------------------------------------
+
+# 1.- Construccion del grafico
+
+HG<-ggplot(penguins, aes(x=largo_aleta_mm))+
+  geom_histogram(col="turquoise4", fill="plum1")+
+  ggtitle("Histograma")+
+  xlab("Largo de la aleta (mm)")+
+  ylab("Frecuencias")+
+  theme_classic()
+
+# 2.- Visualizacion del grafico
+HG
+
+#----------------------------------
+#   Grafico de dispersion
+#-----------------------------------
+
+# 1.- Construccion del grafico
+library(ggplot2)
+ggplot(cars, aes(x = speed, y = dist)) +
+  geom_point(colour = "cyan"")
+
+color=c("deepskyblue")")
+
+GD<-ggplot(penguins, aes(x=largo_pico_mm, y=grosor_pico_mm))+
+  geom_point(aes(color=especie))+
+  ggtitle("Gráfico de dispersión")+
+  xlab("largo del pico (mm)")+
+  ylab("grosor del pico (mm)")+
+  theme_light()
+
+# 2.- Visualizacion del objeto
+GD
+
+#Grafico de disperson 2. Si le quitamos aes(color=especie) quedan tods los puntos en negro.
+
+GD<-ggplot(penguins, aes(x=largo_pico_mm, y=grosor_pico_mm))+
+  geom_point(color="violetred")+
+  ggtitle("Gráfico de dispersión")+
+  xlab("largo del pico (mm)")+
+  ylab("grosor del pico (mm)")+
+  theme_light()
+
+GD
+
+#Grafico 3. Cambio de colores 
+GD<-ggplot(penguins, aes(x=largo_pico_mm, y=grosor_pico_mm))+
+  geom_point(aes(color=especie))+
+  scale_alpha_manual(values=c="cyan", "violetred", "yellow"))+
+  ggtitle("Gráfico de dispersión")+
+  xlab("largo del pico (mm)")+
+  ylab("grosor del pico (mm)")+
+  theme_light()
+
+GD
+GD<-ggplot(penguins, aes(x=largo_pico_mm, y=grosor_pico_mm))+
+  geom_point(aes(color=especie))+
+  scale_color_manual(values=c("cyan", "violetred", "yellow"))+
+  ggtitle("Gráfico de dispersión")+
+  xlab("largo del pico (mm)")+
+  ylab("grosor del pico (mm)")+
+  theme_light()
+GD
+#-------------------------------
+#  Organizacion de graficos
+#-------------------------------
+
+# 1.- Descargar el paquete gridExtra
+
+install.packages("gridExtra")
+
+# 2.- Abrir la libreria
+library(gridExtra)
+
+# 3.- Organizacion 2 graficos en una fila y dos columnas 
+grid.arrange(BX,GB1, nrow=1, ncol=2)
+
+# 4.- Organizacion 3 graficos en dos filas y dos columnas 
+grid.arrange(BX,GB1,HG, nrow=2, ncol=2)
+
+# 5.- Organizacion 4 graficos en dos filas y dos columnas 
+grid.arrange(BX,GB1,HG,GD, nrow=2, ncol=2)
